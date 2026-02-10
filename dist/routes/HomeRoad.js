d3cedf02f1283c6b72fe1fb1931d98fbf4c885a8
Freddyede <136079691+Freddyede@users.noreply.github.com>
2026-02-10 11:14:46 +0100

    Delete dist directory

--- dist/routes/HomeRoad.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("../controllers");
-const homeRoad = (0, express_1.Router)();
-homeRoad.get('/', controllers_1.HomeController.index);
-exports.default = homeRoad;
-//# sourceMappingURL=HomeRoad.js.map
--- dist/utils/response.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jsonResponse = void 0;
-const jsonResponse = (object, res) => {
-    res.status(200).json(object);
-};
-exports.jsonResponse = jsonResponse;
-//# sourceMappingURL=response.js.map
